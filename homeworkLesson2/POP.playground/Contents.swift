682ed99fcef6c2cd87d754f46fcb4d49b693cc2e
goodlvbad <skrivoborodova@yandex.ru>
2022-02-21 16:22:48 +0300

    fixed pop file

--- homeworkLesson2/POP.playground/Contents.swift
+++ homeworkLesson2/POP.playground/Contents.swift
@@ -30,22 +30,6 @@ enum PrimaryAttribute: Int, Comparable {
     }
 }
 
-protocol Statistics {
-    typealias Attributes = (armor: Float, attackPower: Float, strength: Float, agility: Float, intelligence: Float, hitPoints: Float, mana: Float)
-
-    var armor: Float? { get }
-    var attackPower: Float? { get }
-    var strength: Float? { get }
-    var agility: Float? { get }
-    var intelligence: Float? { get }
-    var hitPoints: Float? { get }
-    var mana: Float? { get }
-
-    init(statistics: Attributes, attack style: AttakStyle, attribute: PrimaryAttribute)
-    
-    mutating func updateStatistics(_ statistics: Attributes)
-}
-
 protocol Moveable {
     var currentPosition: CGPoint { get }
     
@@ -86,8 +70,7 @@ extension Actionable {
     }
 }
 
-struct Hero: Statistics, Moveable, Actionable {
- 
+struct Statistics {
     private(set) var armor: Float?
     private(set) var attackPower: Float?
     private(set) var strength: Float?
@@ -95,6 +78,30 @@ struct Hero: Statistics, Moveable, Actionable {
     private(set) var intelligence: Float?
     private(set) var hitPoints: Float?
     private(set) var mana: Float?
+
+    mutating func updateStatistics (
+        armor: Float,
+        attackPower: Float,
+        strength: Float,
+        agility: Float,
+        intelligence: Float,
+        hitPoints: Float,
+        mana: Float)
+    {
+        self.armor = armor
+        self.attackPower = attackPower
+        self.strength = strength
+        self.agility = agility
+        self.intelligence = intelligence
+        self.hitPoints = hitPoints
+        self.mana = mana
+    }
+    
+}
+
+struct Hero: Moveable, Actionable {
+    
+    private(set) var stats: Statistics
     
     private(set) var currentPosition: CGPoint
     
@@ -102,45 +109,24 @@ struct Hero: Statistics, Moveable, Actionable {
     
     private(set) var primaryAttribute: PrimaryAttribute
     
-    init(statistics: Attributes, attack style: AttakStyle, attribute: PrimaryAttribute) {
-        armor = statistics.armor
-        attackPower = statistics.attackPower
-        strength = statistics.strength
-        agility = statistics.agility
-        intelligence = statistics.intelligence
-        hitPoints = statistics.hitPoints
-        mana = statistics.mana
+    init(
+        statistics: Statistics,
+        attack style: AttakStyle,
+        attribute: PrimaryAttribute)
+    {
+        stats = statistics
         action = style
         primaryAttribute = attribute
         currentPosition = CGPoint(x: 0, y: 0)
-    }
-    
-    mutating func updateStatistics(_ statistics: Attributes) {
-        armor = statistics.armor
-        attackPower = statistics.attackPower
-        strength = statistics.strength
-        agility = statistics.agility
-        intelligence = statistics.intelligence
-        hitPoints = statistics.hitPoints
-        mana = statistics.mana
     }
     
     mutating func move(to point: CGPoint) {
         currentPosition = point
         print("move to point \(point)")
     }
-    
 }
 
-var lichStats: Statistics.Attributes
-lichStats.armor = 2
-lichStats.agility = 14
-lichStats.attackPower = 22
-lichStats.hitPoints = 475
-lichStats.intelligence = 20
-lichStats.mana = 300
-lichStats.strength = 15
-
+let lichStats = Statistics(armor: 2, attackPower: 22, strength: 15, agility: 14, intelligence: 20, hitPoints: 475, mana: 300)
 var lich = Hero(statistics: lichStats, attack: .longRange, attribute: .intelligence)
 
 //move lich and attack
@@ -153,48 +139,16 @@ lich.attack()
 
 //let's create more heroes
 
-var archmageStats: Statistics.Attributes
-archmageStats.armor = 3
-archmageStats.agility = 17
-archmageStats.attackPower = 21
-archmageStats.hitPoints = 450
-archmageStats.intelligence = 19
-archmageStats.mana = 285
-archmageStats.strength = 14
-
+let archmageStats = Statistics(armor: 3, attackPower: 21, strength: 14, agility: 17, intelligence: 19, hitPoints: 450, mana: 285)
 var archmage = Hero(statistics: archmageStats, attack: .longRange, attribute: .intelligence)
 
-var shadowHunterStats: Statistics.Attributes
-shadowHunterStats.armor = 4
-shadowHunterStats.agility = 20
-shadowHunterStats.attackPower = 22
-shadowHunterStats.hitPoints = 475
-shadowHunterStats.intelligence = 17
-shadowHunterStats.mana = 255
-shadowHunterStats.strength = 15
-
+let shadowHunterStats = Statistics(armor: 4, attackPower: 22, strength: 15, agility: 20, intelligence: 17, hitPoints: 475, mana: 255)
 var shadowHunter = Hero(statistics: shadowHunterStats, attack: .longRange, attribute: .agility)
 
-var wardenStats: Statistics.Attributes
-wardenStats.armor = 4
-wardenStats.agility = 20
-wardenStats.attackPower = 22
-wardenStats.hitPoints = 550
-wardenStats.intelligence = 16
-wardenStats.mana = 240
-wardenStats.strength = 18
-
+let wardenStats = Statistics(armor: 4, attackPower: 22, strength: 18, agility: 20, intelligence: 16, hitPoints: 550, mana: 240)
 var warden = Hero(statistics: wardenStats, attack: .melee, attribute: .agility)
 
-var alchemistStats: Statistics.Attributes
-alchemistStats.armor = 1
-alchemistStats.agility = 10
-alchemistStats.attackPower = 28
-alchemistStats.hitPoints = 725
-alchemistStats.intelligence = 18
-alchemistStats.mana = 270
-alchemistStats.strength = 25
-
+let alchemistStats = Statistics(armor: 1, attackPower: 28, strength: 25, agility: 10, intelligence: 18, hitPoints: 725, mana: 270)
 var alchemist = Hero(statistics: alchemistStats, attack: .melee, attribute: .strength)
 
 var heroes: [Hero] = []
@@ -212,7 +166,7 @@ print("- attack each hero in heroes -")
 heroes.forEach { $0.attack() }
 
 // sort by attackPower
-heroes.sort { $0.attackPower ?? 0.0 >= $1.attackPower ?? 0.0 }
+heroes.sort { $0.stats.attackPower ?? 0.0 >= $1.stats.attackPower ?? 0.0 }
 
 // sort by attak style (long range > melee)
 heroes.sort { $0.action > $1.action }
@@ -226,7 +180,7 @@ heroes.forEach { print($0.primaryAttribute) }
 
 // max mana of hero in heroes
 func maxMana(of heroes: [Hero]) -> Float {
-    heroes.max(by: { $0.mana ?? 0.0 < $1.mana ?? 0.0 })?.mana ?? 0.0
+    heroes.max(by: { $0.stats.mana ?? 0.0 < $1.stats.mana ?? 0.0 })?.stats.mana ?? 0.0
 }
 maxMana(of: heroes)
 print("max mana of hero in heroes \(maxMana(of: heroes))")
